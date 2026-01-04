8f694e53f751ab0a526c91051fbe5013e87731c5
Andreas Holley <andy.duks@gmail.com>
2026-01-04 18:18:54 +0000

    Implement index for log

--- log/log.go
+++ log/log.go
@@ -4,12 +4,14 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
 type Log struct {
 	LogPath string
+	Index   map[string]int64
 }
 
 type Entry struct {
@@ -18,11 +20,17 @@ type Entry struct {
 }
 
 func (l *Log) Write(key string, value string) error {
-	file, err := os.OpenFile(l.LogPath, os.O_WRONLY|os.O_APPEND, 0644)
+	file, err := os.OpenFile(l.LogPath, os.O_WRONLY, 0644)
 	if err != nil {
 		return fmt.Errorf("error opening file: %v", err)
 	}
 	defer file.Close()
+
+	offset, err := file.Seek(0, io.SeekEnd)
+	if err != nil {
+		return err
+	}
+
 	marshalled, err := json.Marshal(Entry{
 		Key:   key,
 		Value: value,
@@ -37,32 +45,42 @@ func (l *Log) Write(key string, value string) error {
 		return fmt.Errorf("error writing data: %v", err)
 	}
 
+	l.Index[key] = offset
+
 	return nil
 }
 
 func (l *Log) Read(key string) (string, error) {
+	offset, ok := l.Index[key]
+	if !ok {
+		return "", nil
+	}
 	file, err := os.Open(l.LogPath)
 	if err != nil {
 		return "", fmt.Errorf("error opening file: %v", err)
 	}
 	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	latest := ""
-	for scanner.Scan() {
-		var entry Entry
-		if err := json.Unmarshal(scanner.Bytes(), &entry); err != nil {
-			return "", fmt.Errorf("error unmarshalling json: %v", err)
-		}
-		if entry.Key == key {
-			latest = entry.Value
-		}
+	_, err = file.Seek(offset, io.SeekStart)
+	if err != nil {
+		return "", fmt.Errorf("error seeking: %v", err)
 	}
-	return latest, nil
+	reader := bufio.NewReader(file)
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return "", fmt.Errorf("error reading line: %v", err)
+	}
+	var entry Entry
+	err = json.Unmarshal([]byte(line), &entry)
+	if err != nil {
+		return "", fmt.Errorf("error unmarshalling json: %v", err)
+	}
+	return entry.Value, nil
 }
 
 func NewLog(logPath string) *Log {
 	db := &Log{
 		LogPath: logPath,
+		Index:   make(map[string]int64),
 	}
 	_, err := os.Stat(logPath)
 	if os.IsNotExist(err) {
@@ -74,13 +92,16 @@ func NewLog(logPath string) *Log {
 
 		return db
 	}
-	if err = compact(db); err != nil {
+	if err = db.compact(); err != nil {
+		log.Fatal(err)
+	}
+	if err = db.initIndex(); err != nil {
 		log.Fatal(err)
 	}
 	return db
 }
 
-func compact(l *Log) error {
+func (l *Log) compact() error {
 	readFile, err := os.Open(l.LogPath)
 	if err != nil {
 		return fmt.Errorf("error opening file: %v", err)
@@ -118,6 +139,26 @@ func compact(l *Log) error {
 	err = os.Rename(tmpFile.Name(), l.LogPath)
 	if err != nil {
 		return fmt.Errorf("error renaming file: %v", err)
+	}
+	return nil
+}
+
+func (l *Log) initIndex() error {
+	l.Index = make(map[string]int64)
+	file, err := os.Open(l.LogPath)
+	if err != nil {
+		return fmt.Errorf("error opening file: %v", err)
+	}
+	defer file.Close()
+	var offset int64 = 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		var entry Entry
+		if err := json.Unmarshal(scanner.Bytes(), &entry); err != nil {
+			return fmt.Errorf("error unmarshalling json: %v", err)
+		}
+		l.Index[entry.Key] = offset
+		offset += int64(len(scanner.Bytes()) + 1) // +1 byte for /n consumed by scanner
 	}
 	return nil
 }
--- main.go
+++ main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
-	"github.com/dukky/toydb/db"
+	"fmt"
+
 	"github.com/dukky/toydb/log"
 )
 
 func main() {
-	var db db.DB = log.NewLog("test.log")
+	db := log.NewLog("test.log")
 
-	db.Write("Hello", "world")
+	db.Write("Hello", "world\nnewline")
 	db.Write("Goodbye", "world")
+	db.Write("Hello1", "universe")
+	db.Write("Hello2", "everyone")
 
 	value, _ := db.Read("Hello")
 	println("Hello:", value)
 
 	value, _ = db.Read("Goodbye")
 	println("Goodbye:", value)
+
+	fmt.Printf("%#v\n", db.Index)
 }
